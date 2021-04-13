99ce2814440bb2dbbccf3aa24d71516070987fef
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2021-04-13 22:45:36 +0300

    fix url

--- fixture/soap.py
+++ fixture/soap.py
@@ -9,7 +9,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client('http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl')
+        client = Client(self.app.config['web']['base_url'] +'api/soap/mantisconnect.php?wsdl')
         try:
             client.service.mc_login(username, password)
             return True
@@ -17,7 +17,7 @@ class SoapHelper:
             return False
 
     def get_project_list(self, username, password):
-        client = Client('http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl')
+        client = Client(self.app.config['web']['base_url'] + 'api/soap/mantisconnect.php?wsdl')
         list_projects = []
         try:
             projects = list(client.service.mc_projects_get_user_accessible(username, password))
